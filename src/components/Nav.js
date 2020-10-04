51ca05548bcf79d1f4446fc085eb338d9a028632
samueljmillward <samuel.j.pearce@hotmail.com>
2020-10-04 23:59:44 +0100

    Styled Nav and Logo

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,49 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import styled from 'styled-components';
+import Logo from './Logo';
+
+const NavStyles = styled.nav`
+  margin-bottom: 3rem;
+
+  ul {
+    margin: 0;
+    padding: 0;
+    display: grid;
+    grid-template-columns: 1fr 1fr auto 1fr 1fr;
+    grid-gap: 2rem;
+    text-align: center;
+    list-style: none;
+    align-items: center;
+  }
+
+  li {
+    --rotate: -2deg;
+    transform: rotate(var(--rotate));
+    order: 1;
+    &:nth-child(1) {
+      --rotate: 1deg;
+    }
+    &:nth-child(2) {
+      --rotate: -2.5deg;
+    }
+    &:nth-child(4) {
+      --rotate: 2.5deg;
+    }
+    &:hover {
+      --rotate: 3deg;
+    }
+    a {
+      font-size: 3rem;
+      text-decoration: none;
+      display: block;
+      &:hover {
+        color: var(--red);
+      }
+`;
 
 const Nav = () => (
-  <nav>
+  <NavStyles>
     <ul>
       <li>
         <Link to="/">Hot Now</Link>
@@ -11,7 +52,9 @@ const Nav = () => (
         <Link to="/pizzas">Pizza Menu</Link>
       </li>
       <li>
-        <Link to="/">Logo</Link>
+        <Link to="/">
+          <Logo />
+        </Link>
       </li>
       <li>
         <Link to="/slicemasters">Slicemasters</Link>
@@ -20,7 +63,7 @@ const Nav = () => (
         <Link to="/order">Order Ahead!</Link>
       </li>
     </ul>
-  </nav>
+  </NavStyles>
 );
 
 export default Nav;
